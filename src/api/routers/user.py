ca27de455dd38959a06189ed305d516cfedbb800
Ryan Jones <ryan.jones@disneystreaming.com>
2022-03-07 14:42:17 -0500

    fixed structure

---
+++ src/api/routers/user.py
@@ -0,0 +1,27 @@
+from fastapi import status, HTTPException, Depends,APIRouter
+from sqlalchemy.orm import Session
+from src.api import schemas
+from src.api import models, utils
+from src.api.database import get_db
+
+
+router = APIRouter()
+
+
+@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_pwd = utils.hash(user.password)
+    user.password = hashed_pwd
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@router.get("/users/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with {id} not found")
+    return user
